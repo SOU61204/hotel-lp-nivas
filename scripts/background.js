363f312980c1ad19fce09c39675bb9a9f6c5dc99
SOU61204 <souhardya6.12.2004@gmail.com>
2024-07-02 14:36:19 +0530

    Add files via upload

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,16 @@
 const backgrounds = [
-    `url("../images/background/home-background-image-1.jpg")`,
-    `url("../images/background/home-background-image-2.jpg")`,
-    `url("../images/background/home-background-image-3.jpg")`
+    'url("../images/background/home-background-image-1.jpg")',
+    'url("../images/background/home-background-image-2.jpg")',
+    'url("../images/background/home-background-image-3.jpg")'
 ];
 
 function nextBackground()
 {
     const bg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
-    document.querySelector('body').style.background = bg + ` no-repeat center center fixed`;
+    document.querySelector('body').style.background = bg + ' no-repeat center center fixed';
     document.querySelector('body').style.backgroundSize = 'cover';
     document.querySelector('body').style.objectFit = 'cover';
+    document.querySelector('body').style.transition = '15s ease-in-out';
 }
 
 setInterval(nextBackground,20000);
